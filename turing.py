1f220df13386d7ab500d7f27364b9bcf9bedf890
Kaczmarczyk-M <m01kaczmarczyk@gmail.com>
2021-05-30 11:06:21 +0200

    dodane zmiany

--- turing.py
+++ turing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import *
+import logging
 
 class AbstrackMaszynaTuringa(ABC):
     @abstractmethod
@@ -20,18 +21,26 @@ class AbstrackMaszynaTuringa(ABC):
     @abstractmethod
     def WykonajKrok(self):
         pass
+    
 class MaszynaTuringa(AbstrackMaszynaTuringa):
-    def __init__(self, alfabet, symbol_pusty, stany, stan_poczatkowy, przejscia):
+    def __init__(self, tasma, alfabet, symbol_pusty, stany, stan_poczatkowy, przejscia):
+        self.tasma = tasma
         self.alfabet = alfabet 
         self.symbol_pusty = symbol_pusty
         self.stany = stany
         self.stan_poczatkowy = stan_poczatkowy
         self.przejscia = przejscia
     
-    def RozpocznijDzialanieZmodyfikowanejTasmy(self, Tasma):
+    def RozpocznijDzialanieZmodyfikowanejTasmy(self, tasma):
         pass
     
-    def RozpocznijDzialanieBEZZmodyfikowanejTasmy(self, Tasma):
+    def RozpocznijDzialanieBEZZmodyfikowanejTasmy(self, tasma):
+        for i in range(len(tasma)):
+            for j in range(len(MaszynaTuringa.alfabet)):
+                if tasma[i] != MaszynaTuringa.alfabet[j]:
+                    print("zła tasma")
+                    print(logging.FATAL)
+                    break
         pass
     
     def CzyStanPoczątkowy(self, stan):
@@ -41,10 +50,33 @@ class MaszynaTuringa(AbstrackMaszynaTuringa):
         pass
     def WykonajKrok(self):
         pass
+    def doznaku(self, tasma, indeks, znakibrake, oILE, kierunek): #kierunek -1 => lewo; 1 => prawo 
+        a = 0
+        for i in range(oILE):
+            for n in range(len(znakibrake)):
+                if tasma[indeks] == znakibrake[n]:
+                    a = 1
+                    break
+            if a != 1: indeks += kierunek
+        return tasma
 
-
+    def zamiana(self, tasma, indeks, znakPocz, znakKonc, kierunek):
+        if znakPocz == tasma[indeks]: 
+            tasma[indeks] = znakKonc
+            indeks += kierunek 
+            return tasma
+        else:
+            print("Error - zamiana")
+            print(logging.FATAL)
+    def petlaZamian(self, indeks, K, kierunek):
+        for i in range(K): #liczba cyfr
+            indeks += kierunek
+        return indeks
+    
 if __name__ == "__main__":
-    alfabet = [0,1,2,3,4,5,6,7,8,9, ',','-']
-    symbol_pusty = '#'
+    alfabet = [0,1,2,3,4,5,6,7,8,9, ',','-', 'X']
+    symbol_pusty = 'A'
+    tasma = ['A',1,0,-0,5,'A']
+    stan_poczatkowy = 0 
     Masz = MaszynaTuringa(123, 123 , 123, 123, 123)
     
